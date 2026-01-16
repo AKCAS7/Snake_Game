5cfdc1771abcb107b8a4474b3cd709cb893bbdc4
AKCAS7 <138361450+AKCAS7@users.noreply.github.com>
2026-01-16 23:20:29 +0530

    Update Snake_Game.py

--- Snake_Game.py
+++ Snake_Game.py
@@ -1,19 +1,34 @@
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, time
 
 screen  = Screen()
 screen.setup( width = 600, height = 600 )
 screen.bgcolor("black")
 screen.title(" The Snake Game ")
+screen.tracer(0)
 
 positions = [ (0,0), (-20,0), (-40,0) ]
 
+turtles = []
+game_on = True
+
 for position in positions:
     t = Turtle()
+    t.penup()
     t.shape("square")
     t.color("white")
     t.goto(position)
-    
+    turtles.append(t)
 
+while game_on:
+    for t in turtles:
+        t.forward(20)
+        screen.update()
+        time.sleep(1)
+
+
+
+        
+screen.exitonclick()
 
 
 
@@ -28,6 +43,7 @@ for position in positions:
 
 
 screen.exitonclick()
+
 
 
 
