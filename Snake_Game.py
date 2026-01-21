77bb4a9e4bf57cfb2f886abe83752a1171404e10
AKCAS7 <138361450+AKCAS7@users.noreply.github.com>
2026-01-21 10:30:07 +0530

    Update Snake_Game.py

--- Snake_Game.py
+++ Snake_Game.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from snake import Snake
 import time
 
 screen  = Screen()
@@ -12,26 +13,16 @@ starting_positions = [ (0,0), (-20,0), (-40,0) ]
 turtles = []
 game_on = True
 
-for position in starting_positions:
-    new_turtle = Turtle()
-    new_turtle.shape("square")
-    new_turtle.penup()
-    new_turtle.color("white")
-    new_turtle.goto(position)
-    turtles.append(new_turtle)
+snake = Snake()
 
 while game_on:
     screen.update()
     time.sleep(0.1)
-    
-    for t_num in range( len(turtles) - 1, 0, -1 ):
-        new_x = turtles[t_num - 1].xcor()
-        new_y = turtles[t_num - 1].ycor() 
-        turtles[t_num].goto(new_x, new_y)
-    turtles[0].forward(20)
+    snake.move_snake()
 
   
 screen.exitonclick()
+
 
 
 
