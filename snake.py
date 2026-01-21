a0b4a035039eae79cd3d8044731c02bbcb1a619c
AKCAS7 <138361450+AKCAS7@users.noreply.github.com>
2026-01-21 11:10:23 +0530

    Update snake.py

--- snake.py
+++ snake.py
@@ -24,3 +24,56 @@ class Snake:
             new_y = self.turtles[t_num - 1].ycor() 
             self.turtles[t_num].goto(new_x, new_y)
         self.turtles[0].forward(MOVE_DISTANCE)
+    
+    def up(self):
+        for t_num in range( len(self.turtles) - 1, 0, -1):
+            new_x = self.turtles[t_num - 1].xcor()
+            new_y = self.turtles[t_num - 1].ycor() 
+            self.turtles[t_num].goto(new_x, new_y)
+            x = self.turtles[0].heading()
+        if x == 0:
+            self.turtles[0].left(90)
+        elif x == 180:
+            self.turtles[0].right(90)
+        elif x == 270:
+            self.turtles[0].left(180)
+
+    def down(self):
+        for t_num in range( len(self.turtles) - 1, 0, -1):
+            new_x = self.turtles[t_num - 1].xcor()
+            new_y = self.turtles[t_num - 1].ycor() 
+            self.turtles[t_num].goto(new_x, new_y)
+            x = self.turtles[0].heading()
+        if x == 0:
+            self.turtles[0].right(90)
+        elif x == 180:
+            self.turtles[0].left(90)
+        elif x == 90:
+            self.turtles[0].right(180)
+
+    def left(self):
+        for t_num in range( len(self.turtles) - 1, 0, -1):
+            new_x = self.turtles[t_num - 1].xcor()
+            new_y = self.turtles[t_num - 1].ycor() 
+            self.turtles[t_num].goto(new_x, new_y)
+            x = self.turtles[0].heading()
+        if x == 0:
+            self.turtles[0].left(180)
+        elif x == 90:
+            self.turtles[0].left(90)
+        elif x == 270:
+            self.turtles[0].right(90)
+
+    def right(self):
+        for t_num in range( len(self.turtles) - 1, 0, -1):
+            new_x = self.turtles[t_num - 1].xcor()
+            new_y = self.turtles[t_num - 1].ycor() 
+            self.turtles[t_num].goto(new_x, new_y)
+            x = self.turtles[0].heading()
+        if x == 90:
+            self.turtles[0].right(90)
+        elif x == 180:
+            self.turtles[0].right(180)
+        elif x == 270:
+            self.turtles[0].left(90)
+            
