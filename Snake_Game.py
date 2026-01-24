c20a4ccfd1d039414a0fecd7d62067199bd1d48f
AKCAS7 <138361450+AKCAS7@users.noreply.github.com>
2026-01-25 03:36:48 +0530

    Update Snake_Game.py

--- Snake_Game.py
+++ Snake_Game.py
@@ -30,10 +30,18 @@ while game_on:
     snake.move_snake()
     if snake.head.distance() < 15:
         food.new_loc()
+        snake.extend()
         scoreboard.increase_score()
+    if(snake.head.xcor() > 280 or snake.head.xcor() < -280 or
+       snake.head.ycor() > 280 or snake.head.ycor() < - 280):
+           game_on = False
+    for turtles in snake.turtles[1:]:
+        if snake.head.distance(turtles) < 10:
+            game_on = False
 
   
 screen.exitonclick()
+
 
 
 
