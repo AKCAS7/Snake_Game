23db5d8519e63099bf2456590802235150b7de8c
AKCAS7 <138361450+AKCAS7@users.noreply.github.com>
2026-01-26 19:15:34 +0530

    Update Snake_Game.py

--- Snake_Game.py
+++ Snake_Game.py
@@ -34,13 +34,14 @@ while game_on:
         scoreboard.increase_score()
     if(snake.head.xcor() > 280 or snake.head.xcor() < -280 or
        snake.head.ycor() > 280 or snake.head.ycor() < - 280):
-           game_on = False
+           scoreboard.reset()
     for turtles in snake.turtles[1:]:
         if snake.head.distance(turtles) < 10:
-            game_on = False
+            scoreboard.reset()
 
   
 screen.exitonclick()
+
 
 
 
