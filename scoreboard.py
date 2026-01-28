0711860f4c046908e8e7c6b30606e4ba12f46f19
AKCAS7 <138361450+AKCAS7@users.noreply.github.com>
2026-01-28 14:53:08 +0530

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -21,6 +21,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open("data.txt", "w") as file:
+                file.write(f"{self.highscore}")
         self.score = 0
         self.update_score()
     
